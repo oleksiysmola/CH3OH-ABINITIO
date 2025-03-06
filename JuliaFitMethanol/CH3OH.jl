07d850d85c3952c1210ab971a3482a9229b0eb08
alex smola <asmola@vincent.theory.phys.ucl.ac.uk>
2025-03-06 11:18:46 +0000

    implemented functional torsion potentials

--- JuliaFitMethanol/CH3OH.jl
+++ JuliaFitMethanol/CH3OH.jl
@@ -75,3 +75,41 @@ function defineSymmetryOperations(case="C3v(M)"::String)::Array{Float64}
     end
     return symmetryOperations
 end
+
+symmetryOperationsTau::Vector{Function} = [
+    tau -> tau
+    tau -> tau + 2*pi/3
+    tau -> tau - 2*pi/3
+    tau -> -tau - 2*pi/3
+    tau -> -tau
+    tau -> -tau + 2*pi/3
+]
+
+function defineInternalCoordinates(zMatrixCoordinates::Vector{Float64})::Vector{Float64}
+    internalCoordinates::Vector{Float64} = zMatrixCoordinates
+    convertToRadians::Float64 = pi/180
+    
+    # Symmeterised dihedrals
+    d23::Float64 = zMatrixCoordinates[12] - zMatrixCoordinates[11]
+    d12::Float64 = zMatrixCoordinates[11] - zMatrixCoordinates[10]
+    d13::Float64 = zMatrixCoordinates[10] - zMatrixCoordinates[12] + 360.0
+    internalCoordinates[10] = (2*d23 - d12 - d13)*convertToRadians/sqrt(6)
+    internalCoordinates[11] = (d12 - d13)*convertToRadians/sqrt(2)
+    # Torsion
+    internalCoordinates[12] = (zMatrixCoordinates[10] + zMatrixCoordinates[11] + zMatrixCoordinates[12])*convertToRadians/3
+    return internalCoordinates
+end
+
+
+function obtainCoordinateMEP(tau::Float64, powersMEP::Matrix{Int64}, parametersMEP::Vector{Float64})::Float64
+    numberOfParametersMEP::Int64 = size(parametersMEP)[1]
+    coordinateMEP::Float64 = 0
+    for i in 1:numberOfParametersMEP
+        if powersMEP[i, 12] >= 0
+            coordinateMEP += parametersMEP[i]*cos(powersMEP[i, 12]*tau)
+        else
+            coordinateMEP += parametersMEP[i]*sin(powersMEP[i, 12]*tau)
+        end
+    end
+    return coordinateMEP
+end
--- JuliaFitMethanol/JuliaFit.jl
+++ JuliaFitMethanol/JuliaFit.jl
@@ -11,6 +11,7 @@ using LinearAlgebra
 
 
 hartreeToWavenumberConversion::Float64 = 219474.63
+convertToRadians::Float64 = pi/180
 
 # gridFilePath::String = "Grids/CBSgrid.txt"
 include("CH3OH.jl")
@@ -57,6 +58,13 @@ for i in 1:numberOfStructuralParameters
     structuralParametersOn[i] = parse(Float64, structuralParameterBlockSplit[i + 1][end - 1])
 end
 
+# How many MEP parameters for each stretch/bend - order of input file matters!
+numberOfParametersRCO::Int64 = sum(occursin.(r"rCO", structuralParameterLabels))
+numberOfParametersROH::Int64 = sum(occursin.(r"rOH", structuralParameterLabels))
+numberOfParametersRCH::Int64 = sum(occursin.(r"rCH", structuralParameterLabels))
+numberOfParametersAHOC::Int64 = sum(occursin.(r"aHOC", structuralParameterLabels))
+numberOfParametersAHCO::Int64 = sum(occursin.(r"aHCO", structuralParameterLabels))
+
 linearParameterBlock::Vector{String} = inputBlocks[2][2:end]
 numberOfLinearParameters::Int64 = size(linearParameterBlock)[1]
 linearParameterBlockSplit::Vector{Vector{SubString{String}}} = split.(linearParameterBlock, r"\s+")
@@ -68,22 +76,27 @@ linearParametersOn::Vector{Int64} = zeros(numberOfLinearParameters)
 
 for i in 1:numberOfLinearParameters
     linearParameters[i] = parse(Float64, linearParameterBlockSplit[i][end])
-    push!(structuralParameterLabels, String(linearParameterBlockSplit[i][1]))
+    push!(linearParameterLabels, String(linearParameterBlockSplit[i][1]))
     linearPowers[i, :] = parse.(Int64, linearParameterBlockSplit[i][2:end-2])
     linearParametersOn[i] = parse(Float64, linearParameterBlockSplit[i][end - 1])
 end
+
+allParameters::Vector{Float64} = vcat(structuralParameters, linearParameters)
 
 gridBlock::Vector{String} = inputBlocks[3][2:end]
 numberOfGridPoints::Int64 = size(gridBlock)[1] 
 gridBlockSplit::Vector{Vector{SubString{String}}} = split.(gridBlock, r"\s+")
 
 grid::Matrix{Float64} = zeros(numberOfGridPoints, numberOfModes)
+gridInternalCoordinates::Matrix{Float64} = zeros(numberOfGridPoints, numberOfModes)
 energies::Vector{Float64} = zeros(numberOfGridPoints)
 
 for i in 1:numberOfGridPoints
     grid[i, :] = parse.(Float64, gridBlockSplit[i][1:numberOfModes])
+    gridInternalCoordinates[i, :] = defineInternalCoordinates(grid[i, :])
     energies[i] = parse(Float64, gridBlockSplit[i][numberOfModes+1])
 end
+
 
 energies = energies.*hartreeToWavenumberConversion
 minimumEnergy = minimum(energies)
@@ -92,7 +105,86 @@ println()
 energies = energies.-minimumEnergy
 
 
+# CH3OH
+function potentialEnergy(internalCoordinates::Vector{Float64}, parameters::Vector{Float64})::Float64
+    # Obtain MEP parameters for each stretch and bend
+    parameterLowerRange::Int64 = 1
+    parameterUpperRange::Int64 = numberOfParametersRCO
+    rCOeq::Float64 = obtainCoordinateMEP(internalCoordinates[end], structuralPowers[1:parameterUpperRange,:], parameters[1:parameterUpperRange])
+    parameterUpperRange += numberOfParametersROH
+    parameterLowerRange += numberOfParametersRCO
+    rOHeq::Float64 = obtainCoordinateMEP(internalCoordinates[end], structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    parameterUpperRange += numberOfParametersRCH
+    parameterLowerRange += numberOfParametersROH
+    rCH1eq::Float64 = obtainCoordinateMEP(internalCoordinates[end], structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    rCH2eq::Float64 = obtainCoordinateMEP(internalCoordinates[end] + 2*pi/3, structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    rCH3eq::Float64 = obtainCoordinateMEP(internalCoordinates[end] + 4*pi/3, structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    parameterUpperRange += numberOfParametersAHOC
+    parameterLowerRange += numberOfParametersRCH
+    aHOCeq::Float64 = obtainCoordinateMEP(internalCoordinates[end], structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    parameterUpperRange += numberOfParametersAHCO
+    parameterLowerRange += numberOfParametersAHOC
+    aHCO1eq::Float64 = obtainCoordinateMEP(internalCoordinates[end], structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    aHCO2eq::Float64 = obtainCoordinateMEP(internalCoordinates[end] + 2*pi/3, structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    aHCO3eq::Float64 = obtainCoordinateMEP(internalCoordinates[end] + 4*pi/3, structuralPowers[parameterLowerRange:parameterUpperRange,:], parameters[parameterLowerRange:parameterUpperRange])
+    parameterLowerRange += numberOfParametersAHCO
 
-# println(inputBlocks)
+    # println(rCOeq)
+    # println(rOHeq)
+    # println(rCH1eq)
+    # println(rCH2eq)
+    # println(rCH3eq)
+    # println(aHOCeq)
+    # println(aHCO1eq)
+    # println(aHCO2eq)
+    # println(aHCO3eq)
 
-# println(grid)
+    xi::Vector{Float64} = zeros(numberOfModes - 1)
+    # Stretches
+    xi[1] = 1 - exp(-parameters[parameterLowerRange]*(internalCoordinates[1] - rCOeq))
+    xi[2] = 1 - exp(-parameters[parameterLowerRange + 1]*(internalCoordinates[2] - rOHeq))
+    xi[3] = 1 - exp(-parameters[parameterLowerRange + 2]*(internalCoordinates[3] - rCH1eq))
+    xi[4] = 1 - exp(-parameters[parameterLowerRange + 2]*(internalCoordinates[4] - rCH2eq))
+    xi[5] = 1 - exp(-parameters[parameterLowerRange + 2]*(internalCoordinates[5] - rCH3eq))
+
+    # Bending
+    xi[6] = (internalCoordinates[6] - aHOCeq)*convertToRadians
+    xi[7] = (internalCoordinates[7] - aHCO1eq)*convertToRadians
+    xi[8] = (internalCoordinates[8] - aHCO2eq)*convertToRadians
+    xi[9] = (internalCoordinates[9] - aHCO3eq)*convertToRadians
+
+    # Dihedrals
+    xi[10] = internalCoordinates[10]
+    xi[11] = internalCoordinates[11]
+    
+    parameterLowerRange += numberOfMorseParameters
+    parameterUpperRange = size(parameters)[1]
+    
+    symmetryOperations::Array{Float64} = defineSymmetryOperations()
+    numberOfSymmetryOperations::Int64 = size(symmetryOperations)[1]
+
+    potential::Float64 = 0.0
+
+    for i in 1:numberOfSymmetryOperations
+        xiTransformed::Vector{Float64} = symmetryOperations[i, :, :]*xi
+        tau::Float64 = symmetryOperationsTau[i](internalCoordinates[12])
+        for j in parameterLowerRange:parameterUpperRange
+            if linearPowers[j - parameterLowerRange + 1, end] >= 0
+                potential += cos(linearPowers[j - parameterLowerRange + 1, end]*tau)*prod(xiTransformed.^linearPowers[j - parameterLowerRange + 1, 1:end-1])*parameters[j]
+            else
+                potential += sin(linearPowers[j - parameterLowerRange + 1, end]*tau)*prod(xiTransformed.^linearPowers[j - parameterLowerRange + 1, 1:end-1])*parameters[j]
+            end
+        end
+    end
+    potential /= 6
+end
+
+# computePotentialEnergy::Function = (zMatrixCoordinates, parameters) -> 
+# println(zMatrixCoordinates)
+# println(parameters)
+# numberOfParametersRCO::Int64 = sum(occursin.(r"rCO", structuralParameterLabels))
+# return zMatrixCoordinates, parameters
+
+
+
+
